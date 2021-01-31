6e5a89a701b49f7d6b53eeea39d20edacbbe5487
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-01-31 14:15:19 -0800

    UPDATE (firebase.js): firebase configs

--- public/firebase.js
+++ public/firebase.js
@@ -1,17 +1,17 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD4hIWsr4OFDkruVGqORICaeCqyUGLVRiM',
-  authDomain: 'viditia-e6fc4.firebaseapp.com',
-  projectId: 'viditia-e6fc4',
-  storageBucket: 'viditia-e6fc4.appspot.com',
-  messagingSenderId: '987179537921',
-  appId: '1:987179537921:web:3f66c06145de0b23371200',
-  measurementId: 'G-SWMZN48X5N'
+  apiKey: 'AIzaSyB1gNGvTB_KWMYeMvfhvbv_kns5kIW35go',
+  authDomain: 'viditia-cf976.firebaseapp.com',
+  projectId: 'viditia-cf976',
+  storageBucket: 'viditia-cf976.appspot.com',
+  messagingSenderId: '489692885650',
+  appId: '1:489692885650:web:ed1596c9e14d6ece8cac4b',
+  measurementId: 'G-2HDTFJWVPH',
 }
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
-
+export default firebaseApp
 // If we do want to use realtime db
 // const db = firebaseApp.firestore();
 // export default db
