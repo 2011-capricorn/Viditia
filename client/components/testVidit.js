b950664073f6358ae2f8c6c589e08979450cfb9c
Derek Louis <thedereklouis@gmail.com>
2021-01-31 18:43:39 -0500

    added testVidit component

---
+++ client/components/testVidit.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import BarChart from './BarChart'
+import PieChart from './PieChart'
+
+import firebase from '../../public/firebase'
+
+const db = firebase.firestore()
+
+db.collection('users')
+  .doc('Qcnh71oMAUn89QMyLq28')
+  .onSnapshot((doc) => console.log('Current data: ', doc.data()))
+
+const testVidit = () => {
+  return (
+    <div>
+      <h1>Test Vidit</h1>
+      {/* <BarChart data={[2, 4, 6, 8]} size={[500, 500]} /> */}
+      <PieChart size={[500, 500]} />
+    </div>
+  )
+}
+
+export default testVidit
