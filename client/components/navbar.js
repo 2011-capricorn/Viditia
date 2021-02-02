9d14bdabbf154f76b9dfec72bee69f2193b92d6c
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-02-02 14:43:58 -0800

    REFACTOR FE: works with new auth components

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -2,9 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
-import {logout} from '../store'
+// import {logout} from '../store'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
+const Navbar = ({isLoggedIn}) => (
   <div>
     <h1>BOILERMAKER</h1>
     <nav>
@@ -12,9 +12,9 @@ const Navbar = ({handleClick, isLoggedIn}) => (
         <div>
           {/* The navbar will show these links after you log in */}
           <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
+          {/* <a href="#" onClick={handleClick}>
             Logout
-          </a>
+          </a> */}
         </div>
       ) : (
         <div>
@@ -31,26 +31,18 @@ const Navbar = ({handleClick, isLoggedIn}) => (
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
-    isLoggedIn: !!state.user.id
+    isLoggedIn: !!state.user,
   }
 }
 
-const mapDispatch = dispatch => {
-  return {
-    handleClick() {
-      dispatch(logout())
-    }
-  }
+const mapDispatch = (dispatch) => {
+  // return {
+  //   handleClick() {
+  //     dispatch(logout())
+  //   }
+  // }
 }
 
-export default connect(mapState, mapDispatch)(Navbar)
-
-/**
- * PROP TYPES
- */
-Navbar.propTypes = {
-  handleClick: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
-}
+export default connect(mapState)(Navbar)
--- client/routes.js
+++ client/routes.js
@@ -1,29 +1,27 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
-import PropTypes from 'prop-types'
-import {Login, Signup, UserHome} from './components'
-import {me} from './store'
+
+import {LoginForm, SignUpForm, UserHome} from './components'
+import {getUser} from './store/user'
 import SignUpVidit from './components/SignUpVidit'
 import SurveyInput from './components/SurveyInput'
 import testVidit from './components/testVidit'
 
-/**
- * COMPONENT
- */
 class Routes extends Component {
   componentDidMount() {
-    this.props.loadInitialData()
+    this.props.getUser()
   }
 
   render() {
     const {isLoggedIn} = this.props
+    console.log('routes user -->', this.props.user)
 
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
+        <Route path="/login" component={LoginForm} />
+        <Route path="/signup" component={SignUpForm} />
         <Route path="/test" component={SignUpVidit} />
         <Route path="/seed" component={SurveyInput} />
         <Route path="/vidit" component={testVidit} />
@@ -34,39 +32,19 @@ class Routes extends Component {
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
-        <Route component={Login} />
+        <Route component={SignUpForm} />
       </Switch>
     )
   }
 }
 
-/**
- * CONTAINER
- */
-const mapState = (state) => {
-  return {
-    // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
-    // Otherwise, state.user will be an empty object, and state.user.id will be falsey
-    isLoggedIn: !!state.user.id,
-  }
-}
+const mapState = (state) => ({
+  isLoggedIn: !!state.user,
+  user: state.user,
+})
 
-const mapDispatch = (dispatch) => {
-  return {
-    loadInitialData() {
-      dispatch(me())
-    },
-  }
-}
+const mapDispatch = (dispatch) => ({getUser: () => dispatch(getUser)})
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
 export default withRouter(connect(mapState, mapDispatch)(Routes))
-
-/**
- * PROP TYPES
- */
-Routes.propTypes = {
-  loadInitialData: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired,
-}
