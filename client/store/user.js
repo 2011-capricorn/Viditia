64568a59200fc4a3533855487db7ad98a5cbb907
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-02-02 14:43:18 -0800

    REFACTOR: user store to interact with firebase auth functionality

--- client/store/user.js
+++ client/store/user.js
@@ -1,67 +1,28 @@
-import axios from 'axios'
-import history from '../history'
+import firebase from '../../public/firebase'
 
-/**
- * ACTION TYPES
- */
 const GET_USER = 'GET_USER'
+const SET_USER = 'SET_USER'
 const REMOVE_USER = 'REMOVE_USER'
 
-/**
- * INITIAL STATE
- */
-const defaultUser = {}
+const defaultUser = ''
 
-/**
- * ACTION CREATORS
- */
-const getUser = user => ({type: GET_USER, user})
+export const getUser = () => {
+  if (firebase.auth().currentUser)
+    return {type: GET_USER, user: firebase.auth().currentUser.uid}
+
+  firebase
+    .auth()
+    .signInAnonymously()
+    .then(({user: {uid}}) => {
+      return {type: GET_USER, user: uid}
+    })
+}
+export const setUser = (user) => ({type: SET_USER, user})
 const removeUser = () => ({type: REMOVE_USER})
 
-/**
- * THUNK CREATORS
- */
-export const me = () => async dispatch => {
-  try {
-    const res = await axios.get('/auth/me')
-    dispatch(getUser(res.data || defaultUser))
-  } catch (err) {
-    console.error(err)
-  }
-}
-
-export const auth = (email, password, method) => async dispatch => {
-  let res
-  try {
-    res = await axios.post(`/auth/${method}`, {email, password})
-  } catch (authError) {
-    return dispatch(getUser({error: authError}))
-  }
-
-  try {
-    dispatch(getUser(res.data))
-    history.push('/home')
-  } catch (dispatchOrHistoryErr) {
-    console.error(dispatchOrHistoryErr)
-  }
-}
-
-export const logout = () => async dispatch => {
-  try {
-    await axios.post('/auth/logout')
-    dispatch(removeUser())
-    history.push('/login')
-  } catch (err) {
-    console.error(err)
-  }
-}
-
-/**
- * REDUCER
- */
-export default function(state = defaultUser, action) {
+export default function (state = defaultUser, action) {
   switch (action.type) {
-    case GET_USER:
+    case SET_USER:
       return action.user
     case REMOVE_USER:
       return defaultUser
