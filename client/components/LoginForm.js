884c4e7a8ef92061668d0342d80857edb78bb865
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-02-05 10:13:59 -0800

    STYLE (LoginForm): basic

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,8 +1,12 @@
+import {Button, Divider, TextField} from '@material-ui/core'
 import React, {useState} from 'react'
 import {connect} from 'react-redux'
-import {loginThunk} from '../store/user'
+import {Link} from 'react-router-dom'
+import {loginThunk, oauthLoginThunk} from '../store/user'
 
-const LoginForm = ({login, history}) => {
+import './styles/LoginForm.css'
+
+const LoginForm = ({login, oauthLogin, history}) => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
@@ -10,33 +14,72 @@ const LoginForm = ({login, history}) => {
   const handleSubmit = async (e) => {
     e.preventDefault()
     setError(await login(email, password))
-    if (!error) history.push('/home')
+    if (!error) history.push('/vidits')
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">Email</label>
-      <input
-        name="email"
-        type="text"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        name="password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      {error !== '' && <p>{error}</p>}
-      <button type="submit">Submit</button>
-    </form>
+    <div className="flex jcc aic container">
+      <form onSubmit={handleSubmit}>
+        <button
+          type="button"
+          onClick={() => setError('OAuth is still in the works!')}
+          className="flex aic login-btn"
+        >
+          <img src="/google.jpg" className="login-img" />
+          <Divider />
+          <p className="login-btn-text">Log in with Google</p>
+        </button>
+        <button
+          type="button"
+          onClick={() => setError('OAuth is still in the works!')}
+          className="flex aic login-btn"
+        >
+          <img src="/facebook.png" className="login-img" />
+          <Divider />
+          <p>Log in with Facebook</p>
+        </button>
+        <button
+          type="button"
+          onClick={() => setError('OAuth is still in the works!')}
+          className="flex aic login-btn"
+        >
+          <img src="/github.png" className="login-img" />
+          <Divider />
+          <p>Log in with GitHub</p>
+        </button>
+        <TextField
+          label="Email"
+          variant="outlined"
+          required
+          fullWidth={true}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          label="Password"
+          variant="outlined"
+          required
+          fullWidth={true}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {error !== '' && <p>{error}</p>}
+        <Button variant="contained" color="primary" type="submit">
+          Login
+        </Button>
+        {/* <button type="submit">Submit</button> */}
+      </form>
+      <br />
+      <div>
+        New user? <Link to="/signup">Sign up!</Link>
+      </div>
+    </div>
   )
 }
 
 const mapDispatch = (dispatch) => ({
   login: (email, password) => dispatch(loginThunk(email, password)),
+  oauthLogin: (type) => dispatch(oauthLoginThunk(type)),
 })
 
 export default connect(null, mapDispatch)(LoginForm)
