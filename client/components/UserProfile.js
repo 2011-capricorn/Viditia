1876d187cc5e1c09621172290755040fddd55633
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-02-08 13:03:29 -0800

    FEAT: beginning of user profile page

--- client/components/UserProfile.js
+++ client/components/UserProfile.js
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {connect} from 'react-redux'
 
-export const UserProfile = ({userKey}) => {
+import {Divider} from '@material-ui/core'
+
+import UserForm from './UserForm'
+import './styles/UserProfile.css'
+
+export const UserProfile = ({userKey, created, allVidit}) => {
+  const [showUserInformation, setShowUserInformation] = useState(true)
+  const userCreated = allVidit.filter((vidit) =>
+    created.includes(vidit.pollKey)
+  )
+
   return (
     <div>
-      <h3>Welcome, {userKey}</h3>
+      <h3 className="tac">User Profile</h3>
+      <div className="flex profile-container">
+        <div className="profile-nav">
+          <p onClick={() => setShowUserInformation(true)}>User Information</p>
+          <p onClick={() => setShowUserInformation(false)}>Created Vidits</p>
+        </div>
+        <Divider orientation="vertical" />
+        <div className="profile-content">
+          {showUserInformation && <UserForm />}
+        </div>
+      </div>
     </div>
   )
 }
 
-const mapState = ({user: {userKey}}) => ({
+const mapState = ({user: {userKey, created}, vidit: {allVidit}}) => ({
   userKey,
+  created,
+  allVidit,
 })
 
 export default connect(mapState)(UserProfile)
