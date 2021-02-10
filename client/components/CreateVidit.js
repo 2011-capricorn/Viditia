012906d8e111988d2b092ed6afdec12de73d770f
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-02-10 10:08:46 -0800

    UPDATE: create vidit form has more fields

--- client/components/CreateVidit.js
+++ client/components/CreateVidit.js
@@ -1,8 +1,12 @@
 import {
   Button,
   FormControl,
+  FormControlLabel,
+  FormLabel,
   InputLabel,
   MenuItem,
+  Radio,
+  RadioGroup,
   Select,
   TextField,
 } from '@material-ui/core'
@@ -12,11 +16,17 @@ import {connect} from 'react-redux'
 import {addViditThunk} from '../store/vidit'
 import {addCreated} from '../store/user'
 
+// eslint-disable-next-line complexity
 const CreateVidit = ({userKey, addNewVidit, addUserCreated, history}) => {
   const [question, setQuestion] = useState('')
   const [type, setType] = useState('')
-  const [min, setMin] = useState(0)
-  const [max, setMax] = useState(0)
+  const [masterLabel, setMasterLabel] = useState('')
+  const [openMaxValue, setOpenMaxValue] = useState(0)
+  const [units, setUnits] = useState('')
+  const [rangeDescription, setRangeDescription] = useState(false)
+  const [rangeLabel1, setRangeLabel1] = useState('') // rangeLabel1
+  const [rangeLabel5, setRangeLabel5] = useState('') // rangeLabel10
+  const [rangeLabel10, setRangeLabel10] = useState('') // rangeLabel5
   const [dataType, setDataType] = useState('String')
   const [error, setError] = useState('')
 
@@ -58,12 +68,26 @@ const CreateVidit = ({userKey, addNewVidit, addUserCreated, history}) => {
   }
 
   const getReturnValue = async () => {
-    const returnValue = {question, type, answers: []}
+    const split = type.split(' ')
+    const returnType =
+      split[0] === 'Multiple' && split[1] > 2 ? 'Multiple +' : type
+    const returnValue = {question, type: returnType, answers: []}
     let pollKey = ''
-    if (type === 'Open')
-      pollKey = await addNewVidit({...returnValue, dataType}, userKey)
-    else if (type === 'Range')
-      pollKey = await addNewVidit({...returnValue, min, max}, userKey)
+    if (type === 'Open') {
+      pollKey =
+        dataType === 'String'
+          ? await addNewVidit({...returnValue, dataType}, userKey)
+          : await addNewVidit(
+              {...returnValue, dataType, max: openMaxValue, units},
+              userKey
+            )
+    } else if (type === 'Range')
+      pollKey = await addNewVidit({...returnValue}, userKey)
+    else if (returnType === 'Multiple +')
+      pollKey = await addNewVidit(
+        {...returnValue, choices, masterLabel},
+        userKey
+      )
     else pollKey = await addNewVidit({...returnValue, choices}, userKey)
     addUserCreated(pollKey)
     history.push('/vidits')
@@ -78,6 +102,8 @@ const CreateVidit = ({userKey, addNewVidit, addUserCreated, history}) => {
       errorExists = checkChoices() || errorExists
     if (!errorExists) getReturnValue()
   }
+
+  console.log(1111, rangeLabel1, rangeLabel10, rangeLabel5)
 
   return (
     <div>
@@ -125,27 +151,59 @@ const CreateVidit = ({userKey, addNewVidit, addUserCreated, history}) => {
 
       {type === 'Range' && (
         <div>
-          <p>What is the range?</p>
+          <p>
+            Range questions are only 1-10. Need more? Use a Free Response type!
+          </p>
+          <FormControl>
+            <p>Is your question measured in quantity or spectrum?</p>
+            <RadioGroup
+              row
+              value={rangeDescription}
+              onChange={(e) => {
+                const value = e.target.value === 'true'
+                setRangeDescription(value)
+              }}
+            >
+              <FormControlLabel
+                value={true}
+                control={<Radio color="primary" />}
+                label="Spectrum"
+              />
+              <FormControlLabel
+                value={false}
+                control={<Radio color="primary" />}
+                label="Quantity"
+              />
+            </RadioGroup>
+          </FormControl>
+        </div>
+      )}
+
+      {type === 'Range' && rangeDescription && (
+        <div>
           <TextField
             fullWidth={true}
             required
-            label="min"
-            value={min}
-            InputProps={{inputProps: {min: 0}}}
-            type="number"
-            onChange={(e) => setMin(e.target.value)}
+            label="1 describes.."
+            value={rangeLabel1}
+            onChange={(e) => setRangeLabel1(e.target.value)}
+          />
+          <TextField
+            fullWidth={true}
+            label="(Optional) 5 describes.."
+            value={rangeLabel5}
+            onChange={(e) => setRangeLabel5(e.target.value)}
           />
           <TextField
             fullWidth={true}
             required
-            label="max"
-            value={max}
-            InputProps={{inputProps: {min: 0}}}
-            type="number"
-            onChange={(e) => setMax(e.target.value)}
+            label="10 describes.."
+            value={rangeLabel10}
+            onChange={(e) => setRangeLabel10(e.target.value)}
           />
         </div>
       )}
+
       {type === 'Open' && (
         <div>
           <FormControl fullWidth={true}>
@@ -158,6 +216,40 @@ const CreateVidit = ({userKey, addNewVidit, addUserCreated, history}) => {
               <MenuItem value="Number">Number</MenuItem>
             </Select>
           </FormControl>
+        </div>
+      )}
+
+      {dataType === 'Number' && (
+        <div>
+          <p>*Graphs are better with a thoughtful max!*</p>
+          <TextField
+            fullWidth={true}
+            required
+            label="What is the maximum value?"
+            value={openMaxValue}
+            min={0}
+            type="number"
+            onChange={(e) => setOpenMaxValue(e.target.value)}
+          />
+          <TextField
+            fullWidth={true}
+            required
+            label="Graph x axis unit"
+            value={units}
+            onChange={(e) => setUnits(e.target.value)}
+          />
+        </div>
+      )}
+
+      {(type === 'Multiple 3' || type === 'Multiple 4') && (
+        <div>
+          <TextField
+            fullWidth={true}
+            required
+            label="How would you describe the choices in one word?"
+            value={masterLabel}
+            onChange={(e) => setMasterLabel(e.target.value)}
+          />
         </div>
       )}
       {error.length > 0 && <p>{error}</p>}
